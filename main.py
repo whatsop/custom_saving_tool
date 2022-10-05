e630dae12389e5bb177238f0d3e0b9fa1e96c673
whatsop <kasimov.arup@gmail.com>
2022-10-05 18:11:27 -0400

    created base class

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import os
 import sys
+import datetime, time
+import subprocess
+
+from PySide2 import QtUiTools, QtWidgets
 
 import hou
+
+
+class SavingReminder(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        UI_FILE = r'D:\PYTHON_ADVANCED_COURSE\tools\saving_reminder_tool\saving_reminder.ui'
+        self.ui = QtUiTools.QUiLoader().load(UI_FILE, parentWidget=self)
+        
+        
+
+
+win = SavingReminder()
+win.show()
 
