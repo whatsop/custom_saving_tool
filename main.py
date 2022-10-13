bf7625d2f2a1ce879cb71ff373dd8741a4ab2867
whatsop <kasimov.arup@gmail.com>
2022-10-13 00:48:34 -0400

    MAKE a change with the UI + added scene function in main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 """
-content : a tool that will pop up every x minutes to ask if you would like to save your current scene, you can add a comment
-which will create a txt file with the comment you have written inside
+content : a custom saving tool
 
 version : 0.1.0
 date : 10/05/2022
 
-how to : 
+how to : start()
 dependencies : PySide2, hou
 
 author : Arup Kasimov
@@ -13,29 +12,31 @@ author : Arup Kasimov
 
 import os
 import sys
-import datetime, time
-import subprocess
+
+
 
 from PySide2 import QtUiTools, QtWidgets, QtCore
 
 import hou
 
 
-class SavingReminder(QtWidgets.QWidget):
+class CustomSavingTool(QtWidgets.QWidget):
+
+    
     def __init__(self):
         super().__init__()
         
+        
+        # UI
         UI_FILE = r'D:\PYTHON_ADVANCED_COURSE\tools\saving_reminder_tool\saving_reminder.ui'
         self.ui = QtUiTools.QUiLoader().load(UI_FILE, parentWidget=self)
         
-
-        # set remind time combo box
-        for remind_time in range(30, 180, 30):
-            self.ui.remindTimeComboBox.addItem(str(remind_time))
-            
-        self.remind_time_value = 30 # by default 30 minutes
-        self.ui.remindTimeComboBox.currentIndexChanged.connect(self.get_remind_time_value)
+        # path line edit
+        CURR_SCENE_PATH = hou.hipFile.path()
+        self.curr_scene_name = hou.hipFile.basename()
+        self.curr_folder_path = CURR_SCENE_PATH.replace(self.curr_scene_name, "")
         
+        self.ui.pathLineEdit.setText(self.curr_folder_path)
         
         self.comment = ""
 
@@ -45,20 +46,21 @@ class SavingReminder(QtWidgets.QWidget):
     def save_file(self):
         self.comment = self.ui.commentTextEdit.toPlainText()
 
-    def get_remind_time_value(self):
-        self.remind_time_value = self.ui.remindTimeComboBox.currentText()
-        print(self.remind_time_value)
 
     def closeEvent(self, event):
         # will make sure to kill the application by unparenting the hou qt main window
         self.setParent(None)
 
 
+
 #-------------------------------------
 
+
+
 def start():
-    win = SavingReminder()
+    win = CustomSavingTool()
     win.setParent(hou.qt.mainWindow(), QtCore.Qt.Window) # parenting with hou qt main window so the application does not close directly just after starting
     win.show()
+    
 
 start()
